638a97045842d983b7f9ad5f7982869b52fe0cad
Maciej Bliziński <maciej.blizinski@gmail.com>
2016-11-17 21:14:16 +0000

    Aktualizacja narzędzia do zepsutych linków.
    W tej prostej formie ono tylko raportuje co jest do zrobienia. To wystarcza.

--- util/diagnose_broken_links.sh
+++ util/diagnose_broken_links.sh
@@ -6,8 +6,10 @@ set -u
 
 umask 022
 
-wget --spider -r -nd -nv -H -l 3 -o run1.01.wget.log -D localhost http://localhost:1313 || true
-< run1.01.wget.log python process_wget_output.py > run1.02.links.log
-grep -B1 'BROKEN LINK' run1.02.links.log > run1.03.broken-links.log
+rm -f "run1.01.wget.log"
+wget --spider -r -nd -nv -H -l 3 -o "run1.01.wget.log" -D localhost http://localhost:1313 || true
+python process_wget_output.py --write "run1.01.wget.log" ../content \
+  > run1.02.links.log
+# grep -B1 'BROKEN LINK' "run1.02.links.log" > "run1.03.broken-links.log"
 # grep -B1 'broken link!' run1.log | grep -v 'broken link!!!' | grep -v '^--' \
 #   | sed -e 's/:$//' | sed -e 's+http://localhost:1313++' > broken.txt
--- util/process_wget_output.py
+++ util/process_wget_output.py
@@ -9,6 +9,15 @@ import re
 import requests
 import urllib.parse
 from bs4 import BeautifulSoup
+from fuzzywuzzy import process
+import pprint
+import json
+
+import hugotools
+
+WHITELIST = set([
+        'http://localhost:1313/forum'
+])
 
 def Process(inlines):
     out = []
@@ -27,7 +36,8 @@ def Process(inlines):
         if line.startswith('Remote file does not exist') and prev is not None:
             # This means that in the previous line we had the broken URL.
             broken_url = re.sub(':$', '', prev.strip())
-            out.append((last_url, broken_url))
+            if broken_url not in WHITELIST:
+                out.append((last_url, broken_url))
         prev = line
     return out
 
@@ -49,11 +59,9 @@ if __name__ == '__main__':
 
     with open(args.input, 'r', errors='ignore') as fd:
         broken_links = Process(fd.readlines())
-    # print('\n'.join(str(x) for x in broken_links))
 
     # Let's make an index of content stuff.
     content_files = []
-    
     for root, dirs, files in os.walk(args.content_dir):
         relroot = root.lstrip(args.content_dir)
         for name in files:
@@ -61,52 +69,28 @@ if __name__ == '__main__':
                 content_files.append(os.path.join(relroot, name))
 
     baseurl = 'http://localhost:1313/'
-    count = 0
     # We don't really know which URL has a broken link. So let's try to fix them
     # all.
+    fix_candidates = {x.lower(): x for x in content_files}
+    fix_ideas = {}
+    broken_link_slugs = []
     for url, broken_link in broken_links:
-        if re.search(r'\.(png|jpg|JPG)$', url):
-            # Graphics take a long time to process and is pointless here.
-            continue
-        req = requests.get(url)
-        soup = BeautifulSoup(req.text, 'html.parser')
-        metatag = soup.find('meta', id='file-source')
-        if metatag:
-            file_source = metatag.get('content')
-        else:
-            print('no file source for %r, but it has a broken link %r' % (url,
-                broken_link))
-            continue
-        print('Processing:', url, file_source, broken_link)
-        # Now we know to edit file_source and that we have a broken link there.
-        file_path = os.path.join(args.content_dir, file_source)
         broken_link_rel = FrontRemove(broken_link, baseurl)
         broken_link_rel_unquoted = urllib.parse.unquote(broken_link_rel)
-        broken_link_rel_unquoted_lower = broken_link_rel.lower()
-        found = []
-        for cf in content_files:
-            cf_lower = cf.lower()
-            # This is where we're guessing what to use
-            if cf_lower.startswith(broken_link_rel_unquoted_lower):
-                found.append(cf)
-        if found:
-            print('We can fix %r by replacing broken link %r (%r?) with one of %r' % (
-                    file_source, broken_link, broken_link_rel, found))
-        else:
-            print('No ideas how to fix %r in %r' % (broken_link_rel_unquoted, file_source))
-            continue
-        with open(file_path, 'r') as fd:
-            md_content = fd.read()
-        new_content = md_content.replace('/' + broken_link_rel_unquoted, '{{< relref "%s" >}}' % found[0])
-        if new_content == md_content:
-            logging.info("Trying to replace %r didn't work", broken_link_rel_unquoted)
-        if args.write:
-            with open(file_path, 'w') as fd:
-                fd.write(new_content)
-        else:
-            logging.info('Running in dry run mode, not writing to %r.',
-                    file_path)
-        count += 1
-        if count > 10:
-            # break
-            pass
+        broken_link_slugs.append(broken_link_rel_unquoted)
+        broken_link_rel_unquoted_lower = broken_link_rel_unquoted.lower()
+        print('BROKEN LINK: %s, %r' % (broken_link, broken_link_rel_unquoted))
+        fix_idea, match_ratio = process.extractOne(
+                broken_link_rel_unquoted,
+                fix_candidates.keys())
+        print('Fix for %r could be %r with ratio %d' % (broken_link_rel,
+            fix_candidates[fix_idea], match_ratio))
+        fix_ideas[broken_link] = (fix_candidates[fix_idea],
+                broken_link_rel_unquoted, match_ratio)
+    print('Writing fix ideas')
+    with open('fix-ideas.json', 'w') as fd:
+        json.dump(fix_ideas, fd, indent=2)
+    with open('broken-link-slugs.json', 'w') as fd:
+        json.dump(broken_link_slugs, fd, indent=2)
+    with open('broken.txt', 'w') as fd:
+        fd.write('\n'.join(broken_link_slugs) + '\n')
